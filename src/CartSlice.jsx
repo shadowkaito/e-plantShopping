50e50342252b8f5b11bf61e86b1764a6b7ff9e40
shadowkaito <nol.31@sfr.fr>
2025-08-20 11:16:04 -0400

    Revert "Ajout de la gestion des etats du reducer: add,remove,update"
    This reverts commit 164773364accb8007eddd10867dc718080578a50.

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,34 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
-    name: 'cart',
-    initialState: {
-        items: [], // Initialize items as an empty array
+  name: 'cart',
+  initialState: {
+    items: [], // Initialize items as an empty array
+  },
+  reducers: {
+    addItem: (state, action) => {
+    
     },
-    reducers: {
-        addItem: (state, action) => {
-            addItem: (state, action) => {
-                const { name, image, cost } = action.payload;
-                const existingItem = state.items.find(item => item.name === name);
-                if (existingItem) {
-                    existingItem.quantity++;
-                } else {
-                    state.items.push({ name, image, cost, quantity: 1 });
-                }
-            },
+    removeItem: (state, action) => {
     },
-        removeItem: (state, action) => {
-            itemToRemove = action.payload
-            state.items = state.items.filter(item => item.name !== itemToRemove);
-        },
-        updateQuantity: (state, action) => {
-            const { name, quantity } = action.payload; 
-            const itemToUpdate = state.items.find(item => item.name === name);
-            if (itemToUpdate) {
-                itemToUpdate.quantity = quantity; 
-            }
-        },
+    updateQuantity: (state, action) => {
+
+    
     },
+  },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
